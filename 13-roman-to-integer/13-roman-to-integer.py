b1513a5d2e2a925b11008be3115aa66e65dba670
Aleksey <112256122+AlekseyChirkin@users.noreply.github.com>
2022-09-11 00:00:38 +0300

    Time: 94 ms (23.29%), Space: 13.9 MB (30.34%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,34 +1,12 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
+        roman_numbers_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
         arabic_number = 0
-        if s.count('IV') > 0:
-            arabic_number += s.count('IV') * 4
-            s = s.replace('IV', '')
-        if s.count('IX') > 0:
-            arabic_number += s.count('IX') * 9
-            s = s.replace('IX', '')
+        
+        for i in range(len(s)-1):
+            if roman_numbers_dict[s[i]] < roman_numbers_dict[s[i + 1]]:
+                arabic_number -= roman_numbers_dict[s[i]]
+            else:
+                arabic_number += roman_numbers_dict[s[i]] 
 
-        if s.count('XL') > 0:
-            arabic_number += s.count('XL') * 40
-            s = s.replace('XL', '')
-        if s.count('XC') > 0:
-            arabic_number += s.count('XC') * 90
-            s = s.replace('XC', '')
-
-        if s.count('CD') > 0:
-            arabic_number += s.count('CD') * 400
-            s = s.replace('CD', '')
-        if s.count('CM') > 0:
-            arabic_number += s.count('CM') * 900
-            s = s.replace('CM', '')
-
-        for i in s:
-            if i == 'I': arabic_number += 1
-            if i == 'V': arabic_number += 5
-            if i == 'X': arabic_number += 10
-            if i == 'L': arabic_number += 50
-            if i == 'C': arabic_number += 100
-            if i == 'D': arabic_number += 500
-            if i == 'M': arabic_number += 1000
-
-        return arabic_number        
+        return arabic_number + roman_numbers_dict[s[-1]]
