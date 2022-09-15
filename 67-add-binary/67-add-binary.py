07ed02bee1447ca0d1f7451281ab88f5bfa3b979
Aleksey <112256122+AlekseyChirkin@users.noreply.github.com>
2022-09-15 18:06:48 +0300

    Time: 54 ms (51.20%), Space: 14 MB (25.80%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,4 +1,3 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        summary = int(a, 2) + int(b, 2)
-        return '{0:b}'.format(summary)
+        return '{0:b}'.format(int(a, 2) + int(b, 2))
