488a07392eed293f220b3cefa15f9ce4eb9e7c18
Aleksey <112256122+AlekseyChirkin@users.noreply.github.com>
2022-09-19 22:49:51 +0300

    Time: 103 ms (21.56%), Space: 14.9 MB (6.69%) - LeetHub

--- 35-search-insert-position/35-search-insert-position.py
+++ 35-search-insert-position/35-search-insert-position.py
@@ -5,4 +5,27 @@ class Solution:
             if nums[i] < target:
                 res = i + 1
         return res
+        
+#         low = 0
+#         high = len(nums) - 1
+#         res = -1
                 
+#         if target <= nums[0]:
+#             res = 0
+#         if target > nums[high]:
+#             res = high + 1
+            
+#         while low <= high:
+#             mid = low + (high - low) // 2
+            
+#             if nums[mid] == target:
+#                 res = mid
+#                 break
+
+#             if nums[mid] < target:
+#                 low = mid + 1
+#             else:
+#                 high = mid - 1
+        
+#         return res
+        
